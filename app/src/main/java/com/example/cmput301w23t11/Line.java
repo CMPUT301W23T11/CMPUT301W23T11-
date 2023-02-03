66841a5c12b4e1564742ca19658360e7fb41b0e8
J-JLucas <jlucas205@protonmail.com>
2023-02-02 19:42:30 -0700

    Added Line

---
+++ app/src/main/java/com/example/cmput301w23t11/Line.java
@@ -0,0 +1,15 @@
+package com.example.group11participation;
+
+public class Line extends Shape {
+    private Integer x;
+    private Integer y;
+    private Integer x2;
+    private Integer y2;
+
+    public Line(Integer x, Integer y, Integer x2, Integer y2) {
+        this.x = x;
+        this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}
