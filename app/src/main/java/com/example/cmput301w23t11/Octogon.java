8b13ea9352a1ecdeccab997b76ed15b556adf74f
Kristina_Anf <anfilofi@ualberta.ca>
2023-02-03 03:01:52 -0700

    added colour

--- app/src/main/java/com/example/cmput301w23t11/Octogon.java
+++ app/src/main/java/com/example/cmput301w23t11/Octogon.java
@@ -3,9 +3,11 @@ package com.example.cmput301w23t11;
 public class Octogon extends Shape {
     private int x;
     private int y;
+    String color = "blue";
 
-    public Octogon(int x, int y) {
+    public Octogon(int x, int y, String color) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
